f6f2c1e8be28f8bc9df6af58bce103f8c7f1c392
rohan aras <rohanaras@gmail.com>
2015-07-01 16:39:15 -0700

    used a modified version of the AddLegTimes.R script to calculate percentages - BROKEN: percentages dont add to 1

--- Access_Analysis_Rproject/R/deadheadPct.R
+++ Access_Analysis_Rproject/R/deadheadPct.R
@@ -1,12 +1,25 @@
+# Modified version of AddLegTimes script that adds a column to a dataframe
+# that contains the percent of the total Run time that each lef took.
+# Only contains a function -- doesn't actually run it unless you ask
+
+
 library(dplyr)
 
-df = read.csv("data/UW_Trip_Data_PassengerC.csv")
+# in case the file hasn't already been read
+dfr = read.csv("data/UW_Trip_Data_PassengerC.csv")
 
+# takes a dataframe with ETAs, ServiceDates, and Runs
+# returns a dataframe after adding LegTimes and PctTime
 getDeadheadPct <- function(dfr) {
-  currentDay = dfr$ServiceDate[1]
+  
+  # fenceposts
+  currentDay = dfr$ServiceDate[1] 
   currentRun = dfr$Run[1]
   ETA = dfr$ETA[1]
   timeTaken = vector(length=nrow(dfr))
+  pctTimeTaken = vector(length=nrow(dfr))
+  totalRouteTime = 0 #time that the current route has taken
+  baseRow = 1 #the row number in which the current run left base
   
   for(i in 1:nrow(dfr)) {
     leftAt = ETA
@@ -14,22 +27,31 @@ getDeadheadPct <- function(dfr) {
     previousDay = currentDay
     previousRun = currentRun
     currentDay = dfr$ServiceDate[i]
-    currentRun = dfr$ServiceRun[i]
+    currentRun = dfr$Run[i]
+    
+    # if row is when bus leaves base the 'previous' segment took 0s
+    # calculates the pct time taken, adds to vector 
     if (previousDay != currentDay) {
+      pctTimeTaken[baseRow:(i-1)] = timeTaken[baseRow:(i-1)]/totalRouteTime      
+      totalRouteTime = 0
       time = 0
     } else if (previousRun != currentRun) {
+      pctTimeTaken[baseRow:(i-1)] = timeTaken[baseRow:(i-1)]/totalRouteTime      
+      totalRouteTime = 0
       time = 0
     } else {
       time = ETA - leftAt 
     }
     timeTaken[i] = time
+    totalRouteTime = totalRouteTime + time
   }
-  return(timeTaken)
+  
+  # adds new vector to dataframe and returns
+  dfr$PctTime <- pctTimeTaken
+  dfr$LegTime <- timeTaken
+  return(dfr)
 }
 
-
-oneDay = filter(df, ServiceDate==ServiceDate[1])
-oneRun = filter(oneDay, Run==Run[1])
 
 
 
