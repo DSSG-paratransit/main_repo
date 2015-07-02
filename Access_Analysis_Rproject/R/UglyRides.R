d48fb6ccb18a3833c6622edfe86af8c272710d21
fineiskid <ffineis@uw.edu>
2015-07-02 15:00:11 -0700

    UglyRides lives

--- Access_Analysis_Rproject/R/UglyRides.R
+++ Access_Analysis_Rproject/R/UglyRides.R
@@ -5,30 +5,80 @@
 # the number of minutes that each leg he was on took/number of clients on the bus
 # then add that to a running total for costOfClientID
 #
+library(timeDate)
 
-#Need to get data set to work with, probably want smaller to test on first
-dataSet <-
-  
-#Set the fixed bus cost per minute
-#Average weighted bus cost is $48.09 per hour, so $0.8015 per minute
-cost_per_minute <- .8015
-#Creates array of all the different service days
- ride_days = unique(dataSet$ServiceDate)
- #Creates array of all the different client IDs
- clients = unique(ride_days$ClientId)
- clientCost <- numeric(length = nrow(clients))
- for(k in 1:length(ride_days)){ 
-    today = AD_56[which(AD_56$ServiceDate==ride_days[k]),]
-    for(currentClient in 1:length(clients)){
-      clientRide <- AD_56[which(AD_56$ClientId == clients[currentClient])]
-      for (j in 1:length(clientRide)){
-        legCost <- (clientRide$legTime[j]/clientRide$numPass[j])*cost_per_minute
-        clientCost[currentClient] <- clientCost[currentClient] + legCost
-    
+dataSet <- read.csv("/Users/fineiskid/Desktop/Access_Analysis_Rproject_local/data/UW_Trip_Data_QC.csv")
+dataSet$ServiceDate <- as.timeDate(as.character(dataSet$ServiceDate))
+cost_per_minute = 0.805
+
+ride_days = unique(dataSet$ServiceDate)
+#Creates array of all the different client IDs
+clients = unique(dataSet$ClientId)
+#Set up dataframe that I'll use
+#I'll need clientCost, clientID, Run, ServiceDate, LatStart, LonStart, LatEnd, LonEnd
+costDF <- data.frame(1:8)
+costDF <- t(costDF)
+colnames(costDF) <- c("ClientCost", "ClientId", "Run", "ServiceDate", "LatStart",
+                      "LonStart", "LatEnd", "LonEnd")
+write.table(costDF, file = "./data/Cost_analysis.csv", col.names = T, sep = ",")
+
+
+
+#clientCost <- numeric(length = nrow(clients))
+for(k in 1:length(ride_days)){
+  today = dataSet[which(dataSet$ServiceDate==ride_days[k]),]
+  legTimes <- diff(today$ETA)
+  today$legTimes <- c(legTimes, NA)
+  clients <- na.omit(unique(today$ClientID))
+  for(kk in 1:length(clients)){
+    instances <- which(today$ClientID==clients[kk]) #gives rows of today that have clientID == currentClient
+    if((length(instances) %% 2 == 0) & (length(instances) !=0)) {
+      for(i in 1:(length(instances)/2)){
+        instances[(i*2)-1] #currentClient gets on
+        clientRide = today[instances[(i*2)-1]:instances[i*2],]
+        rideCost = 0
+        for (j in 1:(nrow(clientRide)-1)){
+          rideCost = rideCost + (clientRide$legTimes[j]/clientRide$TotalPass[j])*(cost_per_minute/60)
+        }
+        
+        newrow = data.frame(rideCost, clients[kk], clientRide$Run[1],
+                             as.character(clientRide$ServiceDate[1]), clientRide$LAT[1], clientRide$LON[1],
+                             clientRide$LAT[nrow(clientRide)], clientRide$LON[nrow(clientRide)])
+        #Put all the information from this run on this day for this client in the data frame
+        write.table(newrow, file = "./data/Cost_analysis.csv", col.names = F, append = T, sep = ",")
       }
     }
- }
- 
- #Appends the client cost as new column to clients
- clients$clientCost <- clientCost
- #To find ugly rides we'll want to to look at clients df
+    if((length(instances) %% 2 != 0) & (length(instances) !=0)){
+      sprintf("WARNING: number of clientID instances for %s on ride %s on day %s is %s", kk, clientRide$Run[1], ride_days[k], length(instances))}
+  }
+}
+
+
+
+###########
+# Open the cost analysis file and analyze CPB info
+cost_data <- read.csv("data/Cost_analysis.csv", header = T)
+cost_data$ServiceDate <- as.timeDate(as.character(cost_data$ServiceDate))
+upperCosts <- cost_data[which(cost_data$ClientCost != Inf & !is.na(cost_data$ClientCost)),]
+CPB_sub <- upperCosts$ClientCost
+hist(CPB_sub, breaks = seq(0, 500, 5), xlab = "Costs per boarding ($)", main = "CPB Distribution"); abline(v = quantile(CPB_sub, 0.9), col = "red",)
+uglyrides <- upperCosts[which(CPB_sub>quantile(CPB_sub,0.9)),]
+upperCosts <- upperCosts[which(upperCosts$ClientCost >quantile(CPB_sub, 0.9)), ]
+
+##########
+# Match the ugly rides with data in overall 4 month file
+dataSet <- read.csv("/Users/fineiskid/Desktop/Access_Analysis_Rproject_local/data/UW_Trip_Data_QC.csv")
+dataSet$ServiceDate <- as.timeDate(as.character(dataSet$ServiceDate))
+ugVec <- numeric(nrows(upperCosts))
+for (kk in 1:nrow(upperCosts)){
+  ugly <- upperCosts[kk,]
+  ugCli <- which(dataSet$ServiceDate == ugly$ServiceDate & dataSet$Run == ugly$Run
+                 & dataSet$ClientID== ugly$ClientId & dataSet$LAT == ugly$LatStart
+                 & dataSet$LON == ugly$LonStart)
+  
+  
+  
+}
+
+
+
