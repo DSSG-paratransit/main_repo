f40ce88519c76c712797adc22fd3f847f4f6f947
fineiskid <ffineis@uw.edu>
2015-07-09 16:13:23 -0700

    URIDs are saved as own class

--- Python_Scripts/get_URIDs.py
+++ Python_Scripts/get_URIDs.py
@@ -20,11 +20,36 @@ def get_URIDs(data, broken_Run, resched_init_time):
         onoff = pickmeup[pickmeup["ClientId"]==cli]
         if onoff.shape[0] == 1:
             rmClients.append(cli)
-    URIDs = pickmeup[~pickmeup["ClientId"].isin(rmClients)]
+    unsched = pickmeup[~pickmeup["ClientId"].isin(rmClients)]
 
-    print("There are %s rides left to be scheduled" % URIDs.shape[0])
+    print("There are %s rides left to be scheduled" % unsched.shape[0])
 
-    return URIDs
+    class URID:
+        def __init__(self, bookingId, run, pickUpCoords, dropOffCoords, windowPickUp, windowDropOff, spaceOn, mobAids):
+            self.bookingId= bookingId
+            self.run = run
+            self.pickUpCoords = pickUpCoords
+            self.dropOffCoords = dropOffCoords
+            self.windowPickUp = windowPickUp
+            self.windowDropOff = windowDropOff
+            self.spaceOn = spaceOn
+            self.mobAids = mobAids
+
+    diffIDs = unsched.BookingId.unique()
+    saveme = []
+
+    #save separate URID's in a list
+    for ID in diffIDs:
+        temp = URID(bookingId = ID, run = broken_Run,
+             pickUpCoords = unsched[unsched["BookingId"]==unsched.BookingId.iloc[0]][["LAT", "LON"]].iloc[0,],
+             dropOffCoords = unsched[unsched["BookingId"]==unsched.BookingId.iloc[0]][["LAT", "LON"]].iloc[1,],
+             windowPickUp = unsched[unsched["BookingId"]==unsched.BookingId.iloc[0]][["Pickupwin"]].iloc[0,],
+             windowDropOff = unsched[unsched["BookingId"]==unsched.BookingId.iloc[0]][["Dropoffwin"]].iloc[1,],
+             spaceOn = unsched[unsched["BookingId"]==unsched.BookingId.iloc[0]][["MobAids"]].iloc[0,],
+             mobAids = unsched[unsched["BookingId"]==unsched.BookingId.iloc[0]][["MobAids"]].iloc[0,])
+        saveme.append(temp)
+
+    return saveme
 
 #TEST THIS FUNCTION:
 #import pandas as pd
@@ -32,7 +57,7 @@ def get_URIDs(data, broken_Run, resched_init_time):
 
 #data_filepath = "/Users/fineiskid/Desktop/Access_Analysis_Rproject_local/data/single_clean_day.csv"
 #data_allday = pd.read_csv(data_filepath, header = True) #import one day's QC'ed data
-#broken_Run = #RunID
+#broken_Run = 508 #RunID
 #resched_init_time = 800*60 #initial time IN SECONDS that we will begin rerouting buses. Idea is like 2hrs from breakdown time.
 
 # URIDs = get_URIDs(data_allday, broken_Run, resched_init_time)
