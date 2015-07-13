bddaafba36ed2c826e16d8b03108e0d5d3440f02
rohan aras <rohanaras@gmail.com>
2015-07-13 11:33:20 -0700

    created checkPickupInsert - needs heavy modification

--- Python_Scripts/checkPickupInsert.py
+++ Python_Scripts/checkPickupInsert.py
@@ -1,16 +1,29 @@
 # still need to update all of the ETAs after the insert
 
 import pandas as pd
+### Pick the best insertion point and place it in an return a busRun dataframe
+### Deals with dwell times somehow. 
+
 import numpy as np
 
-def checkPickupInsert(URID, busRun):
-	subset = busRun.loc[busRun.ETA < URID.pickupWindowEnd,['ETA', 'Lat', 'Lon']]
+def checkPickupInsert(URID, busRun, distanceTable):
+	runs = busRun[busRun.ETA < URID.pickupWindowEnd,['ETA', 'Lat', 'Lon']].index.tolist()
+	for run in runs: 
+
+
+
+
+
+
+
 	for i in subset.index.tolist(): 
 		times = osrm([URID.latPickup, subset.Lat[i], subset.Lat[i + 1]], [URID.lonPickup, subset.Lon[i], subset.Lon[i + 1]])
 		newETA = times[0] + busRun.ETA[i]
 		if newETA < URID.pickupWindowEnd:
 			if newETA + times[1] < busRun.ETA[i + 1] + 1800:
-				newRow = DataFrame({'SchedTime': 'NA', 'ETA' : newETA, 'Lat': URID.latPickup , 'Lon': URID.lonPickup, 'BookingId': URID.BookingId, 'SpaceOn': 'later', 'SpaceOff': 'later', 'Activity': 0,'MobiAidCode': 'later' }) #need to fix those laters
+				newRow = DataFrame({'SchedTime': 'NA', 'ETA' : newETA, 'Lat': URID.latPickup , 
+				'Lon': URID.lonPickup, 'BookingId': URID.BookingId, 'SpaceOn': 'later', 'SpaceOff': 'later', 
+				'Activity': 0,'MobiAidCode': 'later' }) #need to fix those laters
 				toConcat =  [busRun, newRow]
 				newBusRun = concat(toConcat)
 				newBusRun.sort('ETA', inplace=True)
@@ -26,4 +39,4 @@ def checkPickupInsert(URID, busRun):
 	
 	
 	
-	return(newBusRun) # list of busRun dataframes 
+	return(newBusRun)
