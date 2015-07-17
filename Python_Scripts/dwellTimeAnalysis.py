59c7012ee6967d7139cd6b6f1088b7482ec7f92e
rohan aras <rohanaras@gmail.com>
2015-07-16 17:20:02 -0700

    now combined (de)boardings that happen at the same place and time

--- Python_Scripts/dwellTimeAnalysis.py
+++ Python_Scripts/dwellTimeAnalysis.py
@@ -21,10 +21,16 @@ def characterString(c, n):
 		r = r + c
 	return r
 
+def test(x):
+	print(x)
+
 data = pd.read_csv('D:\\Documents\\Repos\\main_repo\\data\\single_clean_day.csv')
 
 # gathers needed data
-data = data[['ServiceDate','Run','ETA','DwellTime','PassOn','PassOff','MobAids']]
+data.Activity = data.Activity.apply(lambda x: int(x))
+# data = data.iloc(np.where((data.Activity == 0) | (data.Activity == 1)))
+data = data[['ServiceDate','Run','ETA','DwellTime','Activity',
+			'MobAids']].loc[(data.Activity == 0) | (data.Activity == 1)]
 allCodes = ['A','AM','AP','AR','BB','CA','CB','CI','CS','CT','H','H1','H2','HD','LI',
 			'MO','N','NR','OR','OX','PEL','PK','SA','SC','ST','SVC','U','V','V1','V2',
 			'WA','WG','WH','WK','WT','WX','0T']
@@ -38,9 +44,17 @@ for code in allCodes:
 # Attempt to fix an error caused in the regression by this 0T
 data.rename(columns={'0T' : 'OT'}, inplace=True)
 
-boardings = data[pd.notnull(data.PassOn)]
+# combines boardings at the same stop
+data = data.groupby(['ServiceDate','Run','ETA','DwellTime','Activity']).sum()
+#data['DwellTime'] = data.index.get_level_values('DwellTime') # HACK
+#data['Activity'] = data.index.get_level_values('Activity') # HACK
+data.reset_index(inplace=True)
+# print(data) # debug
+
+# splits data into boading and deboarding
+boardings = data[data.Activity == 0]
 # print(boardings) # debug
-deboardings = data[pd.isnull(data.PassOn)]
+deboardings = data[data.Activity == 1]
 
 
 ###################################################################
@@ -51,7 +65,7 @@ deboardings = data[pd.isnull(data.PassOn)]
 ###################################################################
 
 # regression for boarding dwell times
-x = ' + '.join(boardings.columns.values[7:])
+x = ' + '.join(boardings.columns.values[6:])
 y = 'DwellTime'
 reg_formula = y + ' ~ ' + x
 # print reg_formula # debug
@@ -61,9 +75,14 @@ lmb = smf.ols(formula=reg_formula, data=boardings).fit()
 # deboarding regression
 lmd = smf.ols(formula=reg_formula, data=deboardings).fit()
 
+output = open("../data/dwell_time_mobaid_regression.txt", 'w')
 top = characterString('#', 78)  + '\n'
 bottom = characterString('-', 78)
 print top + characterString(' ', 34) + 'Boardings\n' + bottom
+output.write(top + characterString(' ', 34) + 'Boardings\n' + bottom)
 print lmb.summary()
+output.write(str(lmb.summary))
 print '\n\n' + top + characterString(' ', 33) + 'Deboardings\n' + bottom
+output.write('\n\n' + top + characterString(' ', 33) + 'Deboardings\n' + bottom)
 print lmd.summary()
+output.write(str(lmd.summary))
