452e9cff5a594cf5c9d720723194483df22482ea
rohan aras <rohanaras@gmail.com>
2015-07-24 10:57:35 -0700

    humanToSeconds now handles more incorrect time formats

--- Python_Scripts/humanToSeconds.py
+++ Python_Scripts/humanToSeconds.py
@@ -5,13 +5,24 @@ import re
 @returns: the passed in value converted to seconds
 '''
 def humanToSeconds(hhmm):
+
+	# Invalid format
 	format = re.compile('\d\d:\d\d')
 	if(format.match(hhmm) is None):
 		raise ValueError('Time provided is in incorrect format')
+	
 	hoursAndSeconds = re.findall('\d\d', hhmm)
 	# print(hoursAndSeconds[0]) # debug
 	# print(hoursAndSeconds[1]) # debug
-	return(int(hoursAndSeconds[0]) * 3600 + int(hoursAndSeconds[1]) * 60)
+
+	hour = int(hoursAndSeconds[0])
+	sec = int(hoursAndSeconds[1])
+
+	# Invalid Time
+	if(hour > 24 or sec > 59):
+		raise ValueError('Time provided is invalid')
+
+	return(hour * 3600 + sec * 60)
 
 # For testing purposes
 print(humanToSeconds(raw_input('Enter a 24h time in HH:MM format: ')))
