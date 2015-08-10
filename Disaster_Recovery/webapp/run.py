dab09eda9b1e1cc841b8dde50aaeace5e5f59ca1
Joseph Hellerstein <jlheller@uw.edu>
2015-08-10 15:53:15 -0700

    Create Preferred Options Page

--- Disaster_Recovery/webapp/run.py
+++ Disaster_Recovery/webapp/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 #from wtforms import Form, BooleanField, TextField, validators
 import csv
 
@@ -11,12 +11,12 @@ def register():
          return request.form['text']
     return render_template('my_text.html')
 
-ROWS = [("A", "B"), (1, 2), (3,4)]
 @app.route("/display", methods=["GET","POST"])
-def display():
+def preferred_options():
   # Displays a table
   if request.method == 'POST':
-    headers, data_rows = read_csv()
+    data_rows = read_csv()
+    session['data_rows'] = data_rows
     bookingid = None
     busid = None
     if request.form.get('bookingid', None) is not None:
@@ -26,41 +26,40 @@ def display():
       busid = request.form['busid']
       is_bus = True
     row_range = range(len(data_rows))
-    header_range = range(len(headers))
-    return render_template('table_display.html', 
+    return render_template('preferred_options.html', 
         bookingid = bookingid,
         busid = busid,
-        parameters = row_range,
-        header_range = header_range,
         row_range = row_range,
-        headers = headers,
         data_rows = data_rows, 
+        last_data_row = len(data_rows) - 1,
         )
 
-  return render_template('table_request.html')
+  return render_template('request.html')
 
 def read_csv(filename='data/preferred_options.csv'):
   # Reads data from the csv file
-  # Return - headers: string of headers
-  #          data_rows: list of rows corresponding to the headers
+  # Return - data_rows: list of rows corresponding to the headers
   with open(filename, 'rb') as csvfile:
-      rows = csv.reader(csvfile, delimiter=',')
-      data_rows = []
-      got_header = False
-      for row in rows:
-        if not got_header:
-          headers = row
-          got_header = True
-        else:
-          data_rows.append(row)
-  return headers, data_rows
+    rows = csv.reader(csvfile, delimiter=',')
+    data_rows = []
+    got_header = False
+    for row in rows:
+      if not got_header:
+        got_header = True
+      else:
+        data_rows.append(row)
+  return data_rows
   
 
-@app.route("/link/<parameter>/<value>", methods=["GET"])
-def link(parameter, value):
-  return "Got to link with parameter %s and value %s" % (parameter, value)
+@app.route("/link/<row>", methods=["GET"])
+def link(row):
+    bookingid = session['data_rows'][int(row)][0]
+    return render_template('alternative_options.html', 
+        bookingid = bookingid,
+        )
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+  app.debug = True
+  app.secret_key = 'F12Zr47j\3yX R~6@H!jmM]Lwf/,?K9'
+  app.run()
