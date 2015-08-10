3dccd896d3cd1a24d3addfd6b6e52d938a485d45
Joseph Hellerstein <jlheller@uw.edu>
2015-08-10 13:59:18 -0700

    Implemented preferred options from a CSV file:

--- Disaster_Recovery/webapp/run.py
+++ Disaster_Recovery/webapp/run.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 #from wtforms import Form, BooleanField, TextField, validators
-#from flaskext.genshi import render_response
+import csv
 
 app = Flask(__name__)
 
@@ -16,29 +16,45 @@ ROWS = [("A", "B"), (1, 2), (3,4)]
 def display():
   # Displays a table
   if request.method == 'POST':
-    rideid = None
+    headers, data_rows = read_csv()
+    bookingid = None
     busid = None
-    if request.form.get('rideid', None) is not None:
-      rideid = request.form['rideid']
+    if request.form.get('bookingid', None) is not None:
+      bookingid = request.form['bookingid']
       is_bus = False
     elif request.form.get('busid', None) is not None:
       busid = request.form['busid']
       is_bus = True
-    headers = ROWS[0]
-    rows = ROWS[1:]
-    row_range = range(len(rows))
+    row_range = range(len(data_rows))
     header_range = range(len(headers))
     return render_template('table_display.html', 
-        rideid = rideid,
+        bookingid = bookingid,
         busid = busid,
         parameters = row_range,
         header_range = header_range,
         row_range = row_range,
         headers = headers,
-        rows = rows, 
+        data_rows = data_rows, 
         )
 
   return render_template('table_request.html')
+
+def read_csv(filename='data/preferred_options.csv'):
+  # Reads data from the csv file
+  # Return - headers: string of headers
+  #          data_rows: list of rows corresponding to the headers
+  with open(filename, 'rb') as csvfile:
+      rows = csv.reader(csvfile, delimiter=',')
+      data_rows = []
+      got_header = False
+      for row in rows:
+        if not got_header:
+          headers = row
+          got_header = True
+        else:
+          data_rows.append(row)
+  return headers, data_rows
+  
 
 @app.route("/link/<parameter>/<value>", methods=["GET"])
 def link(parameter, value):
